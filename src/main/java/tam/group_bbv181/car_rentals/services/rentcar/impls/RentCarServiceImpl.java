90a029a7b744d8c43b48609d26a9b12a1fb635e3
Arthur <arthurtatarin@gmail.com>
2019-10-31 16:50:05 +0200

    RentCarWebController

--- src/main/java/tam/group_bbv181/car_rentals/services/rentcar/impls/RentCarServiceImpl.java
+++ src/main/java/tam/group_bbv181/car_rentals/services/rentcar/impls/RentCarServiceImpl.java
@@ -6,17 +6,61 @@ import tam.group_bbv181.car_rentals.model.RentCar;
 import tam.group_bbv181.car_rentals.repository.CarRepository;
 import tam.group_bbv181.car_rentals.repository.CustomerRepository;
 import tam.group_bbv181.car_rentals.repository.RentCarRepository;
+import tam.group_bbv181.car_rentals.services.car.impls.CarServiceImpl;
+import tam.group_bbv181.car_rentals.services.customer.impls.CustomerServiceImpl;
 import tam.group_bbv181.car_rentals.services.rentcar.interfaces.IRentCarService;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class RentCarServiceImpl implements IRentCarService {
     @Autowired
     RentCarRepository rentCarRepository;
-    CarRepository carRepository;
-    CustomerRepository customerRepository;
 
+    @Autowired
+    CustomerServiceImpl customerService;
+
+    @Autowired
+    CarServiceImpl carService;
+
+    @PostConstruct
+    void init(){
+        rentCarRepository.deleteAll();
+        rentCarRepository.saveAll(new ArrayList<>(
+                Arrays.asList(
+                        new RentCar(
+                                carService.getAll().get(0),
+                                customerService.getAll().get(0),
+                                LocalDate.of(15, Month.JULY,2017),
+                                LocalDate.of(16,Month.AUGUST,2017)
+                        ),
+                        new RentCar(
+                                carService.getAll().get(1),
+                                customerService.getAll().get(1),
+                                LocalDate.of(26, Month.APRIL,2018),
+                                LocalDate.of(1,Month.MAY,2018)
+                        ),
+                        new RentCar(
+                                carService.getAll().get(2),
+                                customerService.getAll().get(2),
+                                LocalDate.of(4, Month.NOVEMBER,2018),
+                                LocalDate.of(19,Month.DECEMBER,2018)
+                        ),
+                        new RentCar(
+                                carService.getAll().get(3),
+                                customerService.getAll().get(3),
+                                LocalDate.of(20, Month.JANUARY,2019),
+                                LocalDate.of(29,Month.MARCH,2019)
+                        )
+
+                )
+        ));
+    }
     @Override
     public List<RentCar> getAll() {
         return rentCarRepository.findAll();
